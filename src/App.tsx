79e62686dd7a829093f9e0f4218299625c6ec231
Hansanguk <hansanguk0222@gmail.com>
2021-09-22 20:37:57 +0900

    [feat] react-router 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Header } from "@/components/Header";
 import { RecoilRoot } from "recoil";
+import { BrowserRouter } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 const App = () => {
   return (
-    <RecoilRoot>
-      <div className="App">
-        <Header />
-      </div>
-    </RecoilRoot>
+    <BrowserRouter>
+      <RecoilRoot>
+        <div className="App">
+          <Header />
+        </div>
+      </RecoilRoot>
+    </BrowserRouter>
   );
 };
 
