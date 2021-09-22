621f427e8b1e265d52e7431f86edb03cc4dbb1eb
Hansanguk <hansanguk0222@gmail.com>
2021-09-22 17:49:50 +0900

    [feat] 메인 대문 헤더 추가

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,49 @@
-// import styled from 'styled-components';
-// import React from 'react';
-// import
+import styled from "styled-components";
+import React from "react";
+import header from "@/images/header.jpg";
 
-// const Container = styled.div`
-//   height: 360px;
-//   @media only screen and (min-height: 768px) {
-//     height: 480px;
-//   }
-//   width: 100%;
-//   background-image: ${url}
-// `
+const Container = styled.div`
+  height: 360px;
+  @media only screen and (min-width: 768px) {
+    height: 480px;
+  }
+  width: 100%;
+  background-image: url(${header});
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const HeaderName = styled.h1`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 25px;
+  @media only screen and (min-width: 768px) {
+    font-size: 30px;
+  }
+  color: ${(props) => props.theme.color.gray4};
+`;
+
+const LinkHomeA = styled.a`
+  text-decoration: none;
+  &:visited {
+    color: ${(props) => props.theme.color.gray4};
+  }
+  &:hover {
+    color: ${(props) => props.theme.color.gray4};
+  }
+  width: 130px;
+  @media only screen and (min-width: 768px) {
+    width: 150px;
+  }
+`;
+
+export const Header = () => (
+  <Container>
+    <LinkHomeA href="/">
+      <HeaderName>아서의 책방</HeaderName>
+    </LinkHomeA>
+  </Container>
+);
